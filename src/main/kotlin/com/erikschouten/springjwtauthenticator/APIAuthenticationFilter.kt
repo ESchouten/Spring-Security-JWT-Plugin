0f8c42adf971871d3101a873a2331da57fd4d697
Erik Schouten <Erik@ErikSchouten.com>
2018-07-05 14:16:24 +0200

    Spring JWT Authenticator

---
+++ src/main/kotlin/com/erikschouten/springjwtauthenticator/APIAuthenticationFilter.kt
@@ -0,0 +1,39 @@
+package com.erikschouten.springjwtauthenticator
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.security.authentication.AuthenticationServiceException
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class APIAuthenticationFilter : AbstractAuthenticationProcessingFilter("/login") {
+
+    init {
+        //Set HTTP status of response on success and on fail
+        setAuthenticationSuccessHandler { _, response, _ ->
+            run {
+                response.status = HttpServletResponse.SC_OK
+                response.contentType = MediaType.APPLICATION_JSON_VALUE
+            }
+        }
+        setAuthenticationFailureHandler { _, response, _ -> response.status = HttpServletResponse.SC_BAD_REQUEST }
+    }
+
+    //Catch /login request, authenticate token generated from credentials extracted from the request
+    override fun attemptAuthentication(request: HttpServletRequest, response: HttpServletResponse): Authentication {
+        if (request.method != "POST") {
+            throw AuthenticationServiceException("Authentication method not supported: " + request.method)
+        }
+
+        val credentials = jacksonObjectMapper().readValue(request.inputStream, AccountCredentials::class.java)
+        val authToken = UsernamePasswordAuthenticationToken(credentials.email, credentials.password)
+
+        return authenticationManager.authenticate(authToken)
+    }
+
+    //Class used to map credentials on
+    class AccountCredentials(val email: String, val password: String)
+}
---
+++ src/main/kotlin/com/erikschouten/springjwtauthenticator/JWTSecurityContextRepository.kt
@@ -0,0 +1,102 @@
+package com.erikschouten.springjwtauthenticator
+
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.SignatureException
+import org.slf4j.LoggerFactory
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.security.web.context.HttpRequestResponseHolder
+import org.springframework.security.web.context.SaveContextOnUpdateOrErrorResponseWrapper
+import org.springframework.security.web.context.SecurityContextRepository
+import org.springframework.web.util.WebUtils
+import java.security.SecureRandom
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+private const val AUTHORIZATION_HEADER = "Authorization"
+private const val HEADER_BEGIN = "Bearer "
+private const val TOKEN_TTL = 30 * 60 * 1000
+private val secret = ByteArray(256).apply { SecureRandom().nextBytes(this) }
+
+class JWTSecurityContextRepository(private val userDetailsService: UserDetailsService) : SecurityContextRepository {
+
+    private val logger = LoggerFactory.getLogger(JWTSecurityContextRepository::class.java)
+
+    override fun loadContext(requestResponseHolder: HttpRequestResponseHolder): SecurityContext {
+        val context = SecurityContextHolder.createEmptyContext()
+
+        try {
+            val jwt = requestResponseHolder.request.getHeader(AUTHORIZATION_HEADER)
+            if (jwt != null) {
+                validateTokenAndExtractEmail(jwt).let { email ->
+                    val userDetails = this.userDetailsService.loadUserByUsername(email)
+                    val authentication =
+                            UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
+                    authentication.details =
+                            WebAuthenticationDetailsSource().buildDetails(requestResponseHolder.request)
+                    context.authentication = authentication
+                }
+            }
+        } catch (ex: SignatureException) {
+            logger.debug("Old/invalid signature detected")
+        } catch (ex: ExpiredJwtException) {
+            logger.debug("Token is expired")
+        } finally {
+            requestResponseHolder.response =
+                    SaveContextAsJWTOnUpdateOrErrorResponseWrapper(requestResponseHolder.response)
+
+            return context
+        }
+    }
+
+    override fun saveContext(context: SecurityContext, request: HttpServletRequest, response: HttpServletResponse) {
+        val responseWrapper =
+                WebUtils.getNativeResponse(response, SaveContextAsJWTOnUpdateOrErrorResponseWrapper::class.java)
+                        ?: throw IllegalStateException("Cannot invoke saveContext on response $response. " +
+                                "You must use the HttpRequestResponseHolder.response after invoking loadContext")
+
+        responseWrapper.saveContext(context)
+    }
+
+    override fun containsContext(request: HttpServletRequest): Boolean = request.getHeader(AUTHORIZATION_HEADER) != null
+
+    private inner class SaveContextAsJWTOnUpdateOrErrorResponseWrapper(private val response: HttpServletResponse) :
+            SaveContextOnUpdateOrErrorResponseWrapper(response, true) {
+
+        public override fun saveContext(context: SecurityContext) {
+            if (!isContextSaved) {
+                val authentication = context.authentication as? UsernamePasswordAuthenticationToken
+                authentication?.name?.let { email ->
+                    val token = "Bearer " + createJWTForEmail(email)
+                    response.setHeader(AUTHORIZATION_HEADER, token)
+                }
+            }
+        }
+
+    }
+
+    private fun createJWTForEmail(email: String): String {
+        val expiryDate = Date(System.currentTimeMillis().plus(TOKEN_TTL))
+
+        // Prepare JWT with claims set
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, secret.copyOf())
+                .compact()
+
+    }
+
+    private fun validateTokenAndExtractEmail(header: String): String {
+        val token = if (header.startsWith(HEADER_BEGIN)) header.substring(7) else header
+
+        return Jwts.parser().setSigningKey(secret.copyOf()).parseClaimsJws(token).body.subject
+    }
+
+}
