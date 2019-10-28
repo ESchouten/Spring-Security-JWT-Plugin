5d20da87b1980380ca60ef32cc44b7ad80e13bb9
Jessie Liauw A Fong <jessielaff@live.nl>
2019-10-28 14:17:34 +0100

    Open on validator

--- src/main/kotlin/com/erikschouten/springjwtauthenticator/validator/Validator.kt
+++ src/main/kotlin/com/erikschouten/springjwtauthenticator/validator/Validator.kt
@@ -4,7 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
 
 @Service
-class Validator {
+open class Validator {
     @Throws(ValidationException::class)
-    fun validate(userDetails: UserDetails) {}
+    open fun validate(userDetails: UserDetails) {}
 }
